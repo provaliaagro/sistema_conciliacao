6bc515fe2bfa3dfce12dbe59bc7643ce56b5e0f2
Acsa <acsa.silveiras@gmail.com>
2025-10-14 23:28:19 -0300

    Testes Iniciais | 14/10/25

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
  # Primeiro commit 🚀
+
+import streamlit as st
+
+# Configuração do site
+st.set_page_config(page_title="Sistema de Conciliação", page_icon="🚀")
+
+st.title("Sistema de Conciliação | Provalia")
+nome = st.text_input("Digite seu nome: ")
+st.write(f"seu nome é {nome}")
+aceita = st.checkbox("Aceita os termos?")
+st.write(aceita)
+
+st.sidebar.title("Teste")
