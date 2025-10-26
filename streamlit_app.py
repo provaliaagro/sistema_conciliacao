55396a3287b30a4b1e93a787915bf934406f2f75
Acsa <acsa.silveiras@gmail.com>
2025-10-26 13:19:25 -0300

    Início da Autenticação

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,27 @@ import streamlit as st
 st.set_page_config(page_title="Sistema de Conciliação", page_icon="🚀")
 
 st.title("Sistema de Conciliação | Provalia")
-nome = st.text_input("Digite seu nome: ")
-st.write(f"seu nome é {nome}")
-aceita = st.checkbox("Aceita os termos?")
-st.write(aceita)
+# nome = st.text_input("Digite seu nome: ")
+# st.write(f"seu nome é {nome}")
+# aceita = st.checkbox("Aceita os termos?")
+# st.write(aceita)
 
-st.sidebar.title("Teste")
+# st.sidebar.title("Teste")
+st.title("Login")
+username = st.text_input("Nome de Usuário", type="default")
+senha = st.text_input("Senha", type="password")
+
+if st.button("Entrar"):
+    users = st.secrets["users"]
+    # Verifica se o nome está cadastrado
+    if username in users:
+        nome_completo, email_cadastrado, senha_cadastrada = users[username]
+        # Compara credenciais
+        if senha == senha_cadastrada:
+            st.success(f"Bem-vindo(a), {nome_completo}!")
+            st.write(f"Email do responsável pela conciliação: **{email_cadastrado}**")
+            # Aqui entra o restante da lógica da conciliação...
+        else:
+            st.error("E-mail ou senha incorretos.")
+    else:
+        st.error("Usuário não encontrado.")
