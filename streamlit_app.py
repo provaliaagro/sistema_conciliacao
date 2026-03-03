8fe222bf84032a020a8af24314f0f4ebed271db4
Acsa <acsa.silveiras@gmail.com>
2026-03-03 13:38:24 -0300

    Solução de Problemas

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,6 +137,7 @@ if st.session_state.df_extrato is None:
                     indices_controle = ["data", "recurso", "contraparte", "valor"]
                     df_controle = pd.read_excel(controle_financeiro, engine="openpyxl", header=0)
                     st.session_state['df_controle'] = df_controle
+                    st.dataframe(df_controle)
                     df_controle = df_controle[["Data", "Recurso", "Contraparte", "Valor"]]
                     df_controle.columns = indices_controle
                     df_controle = func.remover_linhas_vazias(df_controle)
